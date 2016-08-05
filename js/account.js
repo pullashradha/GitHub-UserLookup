59feea77ecfad1ddc4fe0569ae167e7c0fa0a8b8
Shradha Pulla <pullashradha@gmail.com>
2016-08-05 09:08:49 -0700

    Change $.get promise link to view repos for specific user, added for loop to loop through response array to display name & description of each individual repo, seems to only be 30-something repos when the user should have 57

--- js/account.js
+++ js/account.js
@@ -1,11 +1,15 @@
 var apiKey = require ("./../.env").apiKey;
 
 var getRepos = function(usernameInput) {
-  $.get("https://api.github.com/users/" + usernameInput + "?access_token=" + apiKey).then(function(response) {
-    // console.log(response);
-    console.log(response.name);
-    console.log("Number of public repositories = " + response.public_repos);
-
+  $.get("https://api.github.com/users/" + usernameInput + "/repos?access_token=" + apiKey).then(function(response) {
+    console.log(response);
+    for (var i = 0; i < response.length; i ++)
+    {
+      console.log(response[i].name);
+      console.log(response[i].description);
+    }
+    // console.log(response.name);
+    // console.log("Number of public repositories = " + response.public_repos);
   }).fail(function(error) {
     console.log(error.responseJSON.message);
   });
