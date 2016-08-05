29211d7e49cae7fb4cf8f1a3ef2c325e39384459
Shradha Pulla <pullashradha@gmail.com>
2016-08-05 10:34:28 -0700

    Add if statement to getRepos() method to remove unnecessary : when repos don't have descriptions, add slight styling

--- js/account.js
+++ js/account.js
@@ -13,7 +13,12 @@ var getRepos = function(usernameInput) {
   $.get("https://api.github.com/users/" + usernameInput + "/repos?per_page=1000access_token=" + apiKey).then(function(response) {
     for (var i = 0; i < response.length; i ++)
     {
-      $("#show-repo").append("<li>" + response[i].name + ": " + response[i].description + "</li>");
+      if (response[i].description === null || response[i].description === "") {
+        $("#show-repo").append("<li>" + response[i].name + "</li>");
+      }
+      else {
+        $("#show-repo").append("<li>" + response[i].name + ": " + response[i].description + "</li>");
+      }
     }
   }).fail(function(error) {
     console.log (error.responseJSON.message);
