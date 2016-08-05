57790d9603ae2a9e1258114d511b0eae6ea0849d
Shradha Pulla <pullashradha@gmail.com>
2016-08-05 09:36:26 -0700

    Add getUser() method in account.js to give basic user info (name & number of public repos), change link in getRepos() method to show page 2 of repos list, ERROR FOUND: per_page=1000 in $.get link in getRepos() method is not working

--- js/account.js
+++ js/account.js
@@ -1,20 +1,25 @@
 var apiKey = require ("./../.env").apiKey;
 
-var getRepos = function(usernameInput) {
-  $.get("https://api.github.com/users/" + usernameInput + "/repos?access_token=" + apiKey).then(function(response) {
-    console.log(response);
-    for (var i = 0; i < response.length; i ++)
-    {
-      console.log(response[i].name);
-      console.log(response[i].description);
-    }
-    // console.log(response.name);
-    // console.log("Number of public repositories = " + response.public_repos);
+var getUser = function(usernameInput) {
+  $.get("https://api.github.com/users/" + usernameInput + "?access_token=" + apiKey).then(function(response) {
+    console.log (response.name);
+    console.log ("Number of public repositories = " + response.public_repos);
   }).fail(function(error) {
-    console.log(error.responseJSON.message);
+    console.log (error.responseJSON.message);
   });
 };
 
-exports.getRepos = getRepos;
+var getRepos = function(usernameInput) {
+  $.get("https://api.github.com/users/" + usernameInput + "/repos?page=2&per_page=1000access_token=" + apiKey).then(function(response) {
+    for (var i = 0; i < response.length; i ++)
+    {
+      console.log (response[i].name);
+      console.log (response[i].description);
+    }
+  }).fail(function(error) {
+    console.log (error.responseJSON.message);
+  });
+};
 
-// exports.accountModule = Account;
+exports.getUser = getUser;
+exports.getRepos = getRepos;
--- js/form-interface.js
+++ js/form-interface.js
@@ -1,11 +1,12 @@
-// var Account = require ("./../js/account.js").accountModule;
-var getRepos = require ("./../js/account.js").getRepos;
 var apiKey = require ("./../.env").apiKey;
+var getUser = require ("./../js/account.js").getUser;
+var getRepos = require ("./../js/account.js").getRepos;
 
 $(document).ready(function(event) {
   $("#username-form").submit(function(event) {
     event.preventDefault();
     var usernameInput = $("#username-input").val();
+    getUser(usernameInput);
     getRepos(usernameInput);
   });
 });
